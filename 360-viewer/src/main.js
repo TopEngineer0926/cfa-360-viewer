f396d65c624053afa94520cca697b46d272276a4
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-02-04 18:30:00 -0500

    aws

--- 360-viewer/src/main.js
+++ 360-viewer/src/main.js
@@ -5,6 +5,11 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
+import '@aws-amplify/ui-vue';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 Vue.config.productionTip = false;
 
 new Vue({
