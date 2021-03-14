206dfbae431b4b8227692157a17eaea816266389
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-03-14 19:35:13 -0400

    e2e login

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,9 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    console.log('Cypress.version', Cypress.version)
+    cy.get('[data-test="sign-in-username-input"]').first().type("360testaccount");
+    cy.get('[data-test="sign-in-password-input"]').first().type(`'V[U,&5/34fba.<Q?`);
+    cy.get('[data-test="sign-in-sign-in-button"]').first().click()
   });
 });
