3f99bc63070f4ab8b7d46052645c2eac1f5c537c
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-03-27 09:27:35 -0400

    registration

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer app padless>
     <v-col class="text-center" cols="12">
-      © {{ new Date().getFullYear() }} Chick-fil-A, Inc. All rights reserved.
+      © {{ new Date().getFullYear() }} Chick-fil-A, Inc. All rights reserved. 1
     </v-col>
   </v-footer>
 </template>
