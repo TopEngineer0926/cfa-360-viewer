6ad637e38e4cf0bbcaa2b2b77b4aa8ac8eb32aaa
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-04-27 15:39:08 -0400

    Project Admin

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -36,8 +36,46 @@
         </v-card>
       </v-tab-item>
       <v-tab-item>
-        <v-card flat>
-          <v-card-text>2</v-card-text>
+        <v-card flat v-if="panos">
+          <v-card-text>
+            <v-select
+              :items="panos"
+              v-model="selectedProjectID"
+              item-text="title"
+              item-value="id"
+              label="Select a project"
+              solo
+              @change="getProjectPermission"
+            ></v-select>
+            <v-data-table
+              v-if="selectedProjectID"
+              :headers="projectPermissionHeaders"
+              :items="INDEXusers"
+              :search="search"
+              :items-per-page="500"
+            >
+              <template v-slot:[`item.projectAdmin`]="{ item }">
+                <v-switch
+                  v-model="item.projectAdmin"
+                  @change="addProjectPermission(item, 'admin')"
+                ></v-switch>
+              </template>
+
+              <template v-slot:[`item.projectEditor`]="{ item }">
+                <v-switch
+                  v-model="item.projectEditor"
+                  @change="addProjectPermission(item, 'editor')"
+                ></v-switch>
+              </template>
+
+              <template v-slot:[`item.projectViewer`]="{ item }">
+                <v-switch
+                  v-model="item.projectViewer"
+                  @change="addProjectPermission(item, 'viewer')"
+                ></v-switch>
+              </template>
+            </v-data-table>
+          </v-card-text>
         </v-card>
       </v-tab-item>
       <v-tab-item>
@@ -55,8 +93,14 @@ import { mapState } from "vuex";
 import {
   createSitePermission,
   deleteSitePermission,
+  createProjectPermission,
+  updateProjectPermission,
 } from "../graphql/mutations";
-import { listSitePermissions } from "../graphql/queries";
+import {
+  listPanos,
+  listSitePermissions,
+  getProjectPermission,
+} from "../graphql/queries";
 
 // import { updatePano } from "../graphql/mutations";
 // import { nanoid } from "nanoid";
@@ -82,6 +126,25 @@ export default {
         { text: "Status", value: "status" },
         { text: "Set as Pano App Admin", value: "siteAdmin" },
       ],
+
+      panos: null,
+      selectedProjectID: null,
+      projectPermissionHeaders: [
+        {
+          text: "Name",
+          align: "start",
+          value: "fullname",
+        },
+        { text: "Email", value: "email" },
+        { text: "Company", value: "company" },
+        { text: "Permission", value: "permission" },
+        { text: "Status", value: "status" },
+        { text: "Set as Pano Project Admin", value: "projectAdmin" },
+        { text: "Set as Pano Project Editor", value: "projectEditor" },
+        { text: "Set as Pano Project Viewer", value: "projectViewer" },
+      ],
+      // panosCategoryList: [],
+      // panosTitleList: [],
     };
   },
   computed: mapState(["user"]),
@@ -121,14 +184,100 @@ export default {
         );
       }
     },
+    loadPanos() {
+      API.graphql(graphqlOperation(listPanos)).then((data) => {
+        this.panos = data.data.listPanos.items;
+        // this.panosCategoryList = this.panos.map((pano) => pano.category);
+        // this.panosTitleList = this.panos.map((pano) => pano.title);
+      });
+    },
+    async getProjectPermission() {
+      this.projectPermission = (
+        await API.graphql(
+          graphqlOperation(getProjectPermission, { id: this.selectedProjectID })
+        )
+      ).data.getProjectPermission;
+      console.log("projectPermission", this.projectPermission);
+      if (this.projectPermission == null) {
+        this.projectPermission = {
+          id: this.selectedProjectID,
+          admins: [],
+          editors: [],
+          viewers: [],
+        };
+
+        await API.graphql(
+          graphqlOperation(createProjectPermission, {
+            input: this.projectPermission,
+          })
+        );
+      }
+      this.updateProjectPermissionTable();
+    },
+    updateProjectPermissionTable() {
+      this.INDEXusers.forEach((user) => {
+        user.projectAdmin = this.projectPermission.admins.includes(
+          user.username
+        )
+          ? true
+          : false;
+        user.projectEditor = this.projectPermission.editors.includes(
+          user.username
+        )
+          ? true
+          : false;
+        user.projectViewer = this.projectPermission.viewers.includes(
+          user.username
+        )
+          ? true
+          : false;
+      });
+      this.$forceUpdate();
+    },
+    async addProjectPermission(user, type) {
+      if (type == "admin") {
+        if (user.projectAdmin) {
+          this.projectPermission.admins.push(user.username);
+        } else {
+          this.projectPermission.admins = this.projectPermission.admins.filter(
+            (username) => username !== user.username
+          );
+        }
+      } else if (type == "editor") {
+        if (user.projectEditor) {
+          this.projectPermission.editors.push(user.username);
+        } else {
+          this.projectPermission.editors = this.projectPermission.editors.filter(
+            (username) => username !== user.username
+          );
+        }
+      } else if (type == "viewer") {
+        if (user.projectViewer) {
+          this.projectPermission.viewers.push(user.username);
+        } else {
+          this.projectPermission.viewers = this.projectPermission.viewers.filter(
+            (username) => username !== user.username
+          );
+        }
+      }
+
+      await API.graphql({
+        query: updateProjectPermission,
+        variables: {
+          input: this.projectPermission,
+        },
+      });
+      this.updateProjectPermissionTable();
+    },
   },
   watch: {
     tab: function (val) {
       if (val == 0) {
-        console.log("Site Admin Tab");
         if (!this.siteAdmins) {
           this.loadSiteAdmin();
         }
+      } else if (val == 1) {
+        this.loadPanos();
       }
     },
   },
