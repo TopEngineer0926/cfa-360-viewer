f1f8153cb4bb91d31c2b24c86da0d0e2eb152d69
TopEngineer0926 <top_engineer_0926@outlook.com>
2021-12-06 13:16:05 -0500

    sameSite

--- src/main.js
+++ src/main.js
@@ -47,7 +47,7 @@ Amplify.configure({
       // // OPTIONAL - Cookie expiration in days
       // expires: 365,
       // // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
-      sameSite: "strict" | "lax",
+      // sameSite: "strict" | "lax",
       // OPTIONAL - Cookie secure flag
       // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
       secure: false
